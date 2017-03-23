c2bb471ed07cb8114a08c7f7f13efd269bc284b9
ZacMillionaire <dev@jotunga.com>
2017-03-24 08:46:11 +1000

    Added Test Area

--- Nulah-Core/Program.cs
+++ Nulah-Core/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
-namespace Nulah_Core
+namespace NulahCore
 {
     public class Program
     {
--- Nulah-Core/Startup.cs
+++ Nulah-Core/Startup.cs
@@ -8,14 +8,20 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Nulah_Core
+namespace NulahCore
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,11 +33,14 @@ namespace Nulah_Core
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStaticFiles();
+            app.UseMvc();
+            /*
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
             });
+            */
         }
     }
 }
